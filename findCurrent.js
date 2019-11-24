35a2fa7a12afc243142ca285222f7c2e4675c6af
Yiyun Zhang <hzzyy1997@gmail.com>
2019-11-24 16:18:48 -0500

    new course numbers

---
+++ findCurrent.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const courseStr = fs.readFileSync("./course_num_clean.txt").toString();
+const courseArr = courseStr.split(',')
+console.log(courseArr.indexOf("school:Steinhardt - Undergraduate"))
